cf31a8365e2bc748a40af2a6d6fd7950c87af0cb
poskudarnov_MAIN_PC <poskudarnov@niitfa.ru>
2025-01-23 17:25:29 +0300

    realized a function to increase a number in string

--- fileNameRedactor.cpp
+++ fileNameRedactor.cpp
@@ -2,17 +2,28 @@
 #include <string>
 #include <filesystem>
 #include <windows.h>
+#include <vector>
 
 using namespace std;
 using namespace filesystem;
 
+void increaseNumberByN(string& s , int numberToReplace, int increaseNum){
+    s.replace(s.find(to_string(numberToReplace)), 1, to_string(numberToReplace+increaseNum));
+}
+
 int main() {
     
+    string s = "123456789";
+    increaseNumberByN(s, 5, 10);
+    cout << s << endl;
+
     string exeName("fileNameRedactor.exe");
     string path = _pgmptr;
     path.erase(path.find(exeName));
     cout << "path variable: " << path << endl;
+
     for (const auto & entry : directory_iterator(path)){
+        
         std::cout << entry.path() << std::endl;
     }
 
