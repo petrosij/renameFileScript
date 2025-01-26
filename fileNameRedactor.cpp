1ee99d5f57f9e009429d55b842831a7bfe7e5b0f
petrosij <skudarnovpetr11@gmail.com>
2025-01-26 15:06:40 +0300

    Decided to try winbase.h function MoveFile. Added a function to read print error message to console. "printErrorMessage". Error: The system cannot find the file specified.

--- fileNameRedactor.cpp
+++ fileNameRedactor.cpp
@@ -1,8 +1,10 @@
 #include <iostream>
 #include <string>
 #include <filesystem>
+#include <fstream>
 #include <windows.h>
 #include <vector>
+#include <cstdio>
 
 using namespace std;
 using namespace filesystem;
@@ -11,9 +13,36 @@ void increaseNumberByN(string& s , int numberToReplace, int increaseNum){
     s.replace(s.find(to_string(numberToReplace)), 1, to_string(numberToReplace+increaseNum));
 }
 
+void printErrorMessage(DWORD errorCode){
+    LPVOID msgBuffer;
+    DWORD  dw = FormatMessage(
+        FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_IGNORE_INSERTS,
+        NULL,
+        errorCode,
+        MAKELANGID(LANG_ENGLISH, SUBLANG_ENGLISH_US),
+        (LPSTR)&msgBuffer,
+        0, NULL);
+
+        if (dw){
+
+            SetConsoleOutputCP(CP_UTF8);
+            //wcout << L"error: " << (wchar_t*)msgBuffer << endl;
+            //wcout << L"Length of message: " << wcslen((wchar_t*)msgBuffer) << " symbols." << endl;
+
+
+            cerr << "Error: " << (char*) msgBuffer << std::endl;
+            LocalFree(msgBuffer);
+        }else{
+            cerr << "Not succeed obtaing error text. Error code: " << (char*) msgBuffer << std::endl;
+        }
+
+}
+
 int main() {
     
     string s = "123456789";
+    int renameStatus = 0;
+
     increaseNumberByN(s, 5, 10);
     cout << s << endl;
 
@@ -23,9 +52,22 @@ int main() {
     cout << "path variable: " << path << endl;
 
     for (const auto & entry : directory_iterator(path)){
-        
-        std::cout << entry.path() << std::endl;
+        cout << entry.path() << endl;
     }
+    
+    std::filesystem::path p = std::filesystem::current_path();
+    const char* oldFilePath = "C:\\src\\projects\\renameFileScript\\1.txt"; 
+    const char* newFilePath = "C:\\src\\projects\\renameFileScript\\11.txt"; 
+    //ofstream fileObject {"C:\\src\\projects\\renameFileScript\\1.txt"};
+    if (MoveFile (oldFilePath, newFilePath)){
+        cout << "Successfully renamed" << endl;
+    }else{
+        DWORD errorCode = GetLastError();
+        cerr << "Error renaming file code: " << errorCode << endl;
+        printErrorMessage(errorCode);
+    }
+    //cout << "renameStatus: " << renameStatus << endl;
+    
 
     return 0;
 }
